f6bf9201f5c94f9635fa08f04c7b23e1d7eada3b
MoritzMo2025 <moritzstrueve.spam@gmail.com>
2025-08-28 21:27:58 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,26 @@
+const INVENTORY = [
+  {
+    "ID": "1",
+    "Name": "Fernweg Evo Pro 63",
+    "Kategorie": "LRS",
+    "Preis": "7499",
+    "Hersteller": "Lightweight",
+    "Zustand": "neu"
+  },
+  {
+    "ID": "2",
+    "Name": "Ultegra Laufradsatz WH-R8170-C60-TL",
+    "Kategorie": "LRS",
+    "Preis": "1249",
+    "Hersteller": "Shimano",
+    "Zustand": "neu"
+  },
+  {
+    "ID": "3",
+    "Name": "Teammachine SLR FRS V1",
+    "Kategorie": "RMN",
+    "Preis": "1759",
+    "Hersteller": "BMC",
+    "Zustand": "neu"
+  }
+];
